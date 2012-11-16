6db11eebe8021ce0f4460a9e8b90dfdcb3a00560
Patz Huang <walkaniceday@gmail.com>
2012-11-16 13:33:44 +0800

    Add CubicControl.

--- CubicClock/View/CubicClockForm.cs
+++ CubicClock/View/CubicClockForm.cs
@@ -2,11 +2,28 @@
 
 namespace CubicClock.View
 {
-    public partial class CubicClockForm : Form
+    public partial class CubicClockForm : Form, IView
     {
         public CubicClockForm()
         {
             InitializeComponent();
+        }
+
+        public Presenter Presenter
+        {
+            set { throw new System.NotImplementedException(); }
+        }
+
+        public int Hour
+        {
+            get { throw new System.NotImplementedException(); }
+            set { throw new System.NotImplementedException(); }
+        }
+
+        public int Minute
+        {
+            get { throw new System.NotImplementedException(); }
+            set { throw new System.NotImplementedException(); }
         }
     }
 }
---
+++ CubicClock/View/CubicControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace CubicClock.View
+{
+    public partial class CubicControl : UserControl
+    {
+        private int _value;
+
+        public CubicControl()
+        {
+            InitializeComponent();
+        }
+
+        public int Max { get; set; }
+
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+                Update();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            var blackRegion = new RectangleF(0, 0, Value/Max*Width, Height);
+            using (var blackBrush = new SolidBrush(Color.Black))
+            {
+                e.Graphics.FillRectangle(blackBrush, blackRegion);
+            }
+        }
+    }
+}
